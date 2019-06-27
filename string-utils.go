97f79a757b774d965e3bf0bad8f486ab0101b8da
Joffrey Chambrin <joffrey.chambrin@gmail.com>
2019-06-27 19:07:38 +0200

    first commi

---
+++ string-utils.go
@@ -0,0 +1,17 @@
+package string_utils
+
+import "strings"
+
+// After substring after
+func After(value string, a string) string {
+	// Get substring after a string.
+	pos := strings.LastIndex(value, a)
+	if pos == -1 {
+		return ""
+	}
+	adjustedPos := pos + len(a)
+	if adjustedPos >= len(value) {
+		return ""
+	}
+	return value[adjustedPos:len(value)]
+}
